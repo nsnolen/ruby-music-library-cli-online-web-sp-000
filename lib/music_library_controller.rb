34b698d22540460c82dba8ce97663abb463247ae
Sara Nolen <nolensara1@gmail.com>
2021-03-02 00:13:36 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,7 +21,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    
+
     case users_input
       when "list songs"
         list_songs
