b63b9ba79fe814f587e519898397aeb5f189dc34
Sara Nolen <nolensara1@gmail.com>
2021-03-02 00:06:25 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,10 +24,14 @@ class MusicLibraryController
     case users_input
       when "list songs"
         list_songs
-      when "list songs_by_artists"
+      when "list artists"
         list_artists
-      when "list song_by_genres"
+      when "list genres"
         list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
       when "play song"
         play_song
       end
